b889105dcbb92754cbb41baf6dfdc80284d726a3
Adam <06adam.sal@gmail.com>
2024-02-06 21:49:14 -0600

    Day 1 - 10

---
+++ src/day11_practice_tasks/CarpetClient.java
@@ -0,0 +1,16 @@
+package day11_practice_tasks;
+
+public class CarpetClient {
+
+    public static void main(String[] args) {
+
+        Carpet carpet1 = new Carpet(12, 12, 300, true);
+
+        Carpet carpet2 = new Carpet(25, 40, 25, false);
+
+        System.out.println(carpet1);
+        System.out.println(carpet2);
+
+    }
+
+}
