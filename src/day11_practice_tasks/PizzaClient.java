c4aeb755c0d2e97c588ef5054b16ed3431880ede
Adam <06adam.sal@gmail.com>
2024-02-06 21:14:04 -0600

    Day 1 - 10

---
+++ src/day11_practice_tasks/PizzaClient.java
@@ -0,0 +1,22 @@
+package day11_practice_tasks;
+
+public class PizzaClient {
+
+    public static void main(String[] args) {
+
+        Pizza pizza1 = new Pizza("Small", 0, 0);
+
+        Pizza pizza2 = new Pizza("Medium", 10, 25);
+
+        Pizza pizza3 = new Pizza("Large", 99, 99);
+
+        System.out.println(pizza1);
+
+        System.out.println(pizza2);
+
+        System.out.println(pizza3);
+
+
+    }
+
+}
