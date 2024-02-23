12adc2316739243ed3fa28b2510287d67cc10bdb
Adam <06adam.sal@gmail.com>
2024-02-23 01:07:56 -0600

    Tasks

---
+++ src/day15_practice_tasks/CommonElements.java
@@ -0,0 +1,48 @@
+package day15_practice_tasks;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CommonElements {
+
+    public static void main(String[] args) {
+
+        Set<Integer> set1 = new TreeSet<>();
+        set1.addAll(Arrays.asList(
+                10, 8, 11, 20, 5, 0
+        ));
+
+
+        Set<Integer> set2 = new TreeSet<>();
+        set2.addAll(Arrays.asList(
+                30, 0, 20, 40, 50, 10, 60
+        ));
+
+
+        set1.retainAll(set2);
+        System.out.println(set1);
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+/*
+ Create a class named CommonElements and write a program that identifies the common elements in two given sets of integers and returns a third set containing these common elements.
+
+         Example:
+            set1 = {10, 8, 11, 20, 5, 0}
+            set2 = {30, 0, 20, 40, 50, 10, 60}
+
+         Output:
+            set3 = {0, 10, 20}
+
+ */
---
+++ src/day15_practice_tasks/RemoveDuplicates.java
@@ -0,0 +1,42 @@
+package day15_practice_tasks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.*;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list =  new ArrayList<>();
+        list.addAll(asList(
+                20, 15, 10, 20, 30, 10, 20, 15, 0
+        ));
+
+
+        List<Integer> uniqueList = new ArrayList<>();
+
+        for (Integer each : list) {
+            if (uniqueList.contains(each)) {
+                continue;
+            }
+            uniqueList.add(each);
+        }
+        Collections.sort(uniqueList);
+        System.out.println(uniqueList);
+    }
+}
+
+
+/*
+1. Create a class named RemoveDuplicates and write a program that takes a list of integers, removes any duplicate values, and returns the elements in sorted order.
+
+         Example:
+            {20, 15, 10, 20, 30, 10, 20, 15, 0}
+
+         Output:
+            {0, 10, 15, 20, 30}
+ */
